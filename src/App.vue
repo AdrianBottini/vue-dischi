9504afe9accbe8918bed44408d17edf8cee3253d
AdrianBottini <adrianboh@gmail.com>
2022-12-05 11:40:38 +0100

    config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header-page @changeGenre="searchGenre" />
+    <main-page :arr-albums="arrSelectedGenre" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+import HeaderPage from '@/components/HeaderPage.vue';
+import MainPage from '@/components/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HeaderPage,
+    MainPage,
+  },
+  data() {
+    return {
+      urlApi: 'https://flynn.boolean.careers/exercises/api/array/music',
+      Albums: [],
+      genreTwo: 'All',
+    };
+  },
+  computed: {
+    arrSelectedGenre() {
+      return this.Albums.filter((objAlbum) => objAlbum.genre === this.genreTwo);
+    },
+  },
+  created() {
+    axios.get(this.urlApi)
+      .then((axiosResponse) => {
+        console.log(axiosResponse);
+        this.Albums = axiosResponse.data.response;
+      });
+  },
+  methods: {
+    searchGenre(genre) {
+      this.genreTwo = genre;
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <img src="../img/logo-small.svg" alt="">
+    <label for="select-genre">
+      <select id="select-genre" v-model="selectValue"
+      name="genre"
+      @change="$emit('changeGenre', selectValue)">
+        <option value="All">All</option>
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+      </select>
+    </label>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+  data() {
+    return {
+      selectValue: 'All',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 10vh;
+  background-color: grey;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+
+  img {
+    height: 100%;
+    margin-right: 2rem;
+  }
+
+  select {
+    width: 100px;
+  }
+}
+</style>
